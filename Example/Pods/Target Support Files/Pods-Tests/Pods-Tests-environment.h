098602e1044368bf105aeb56f922bcb4d69022d5
Lucas Martins <lucoceano@ckl.io>
2015-02-04 17:22:18 -0200

    Creates library on Cocoa Pods

---
+++ Example/Pods/Target Support Files/Pods-Tests/Pods-Tests-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// CKDraggableModalController
+#define COCOAPODS_POD_AVAILABLE_CKDraggableModalController
+#define COCOAPODS_VERSION_MAJOR_CKDraggableModalController 0
+#define COCOAPODS_VERSION_MINOR_CKDraggableModalController 1
+#define COCOAPODS_VERSION_PATCH_CKDraggableModalController 0
+
